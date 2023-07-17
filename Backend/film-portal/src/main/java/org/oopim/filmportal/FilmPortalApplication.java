257a1eddbe859e722063ad016ed7fb9aa662c07c
Danilo Korać <72507305+danilokorac@users.noreply.github.com>
2023-07-17 22:18:33 +0200

    Initialized backend for project

---
+++ Backend/film-portal/src/main/java/org/oopim/filmportal/FilmPortalApplication.java
@@ -0,0 +1,13 @@
+package org.oopim.filmportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmPortalApplication.class, args);
+	}
+
+}
---
+++ Backend/film-portal/src/test/java/org/oopim/filmportal/FilmPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oopim.filmportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
